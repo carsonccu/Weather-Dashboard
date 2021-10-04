2af6e0fc133671bcdd56192a1adfada4c6287499
Carson <carsoncolgateccu@gmail.com>
2021-10-04 11:56:34 -0600

    started working on my functions in javascript

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,17 @@
 var APIKey = "&appid=348695b7be825684de6066f96d8b32f6";
 var userInput = document.querySelector("#city");
-var searchButton = document.querySelector("#search");
+var searchButton = document.querySelector("#btn btn-primary btn-lg btn-block");
+var city = "New York"
+
+var starterUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+
+function search(e) {
+    userInput = userInput.value;
+    currentweather(e)
+}
+function currentweather(e) {
+    e.preventDefault();
+
+}
+
+searchButton.addEventListener("click", currentweather);
