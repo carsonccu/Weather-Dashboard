3ac2c0d860e12b8ae5d74a2669f5e3566e12b2fa
Carson <carsoncolgateccu@gmail.com>
2021-10-06 10:32:44 -0600

    added a section for current weather and 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -20,30 +20,30 @@ function currentweather(e) {
 
 
 
-fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=London%2Cuk&lat=0&lon=0&callback=test&id=2172797&lang=null&units=imperial&mode=xml", {
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-        "x-rapidapi-key": "8014487f1emshaa274c05a26e32ap1b8b54jsncaca7949dc19"
-    }
-})
-    .then(response => {
-        console.log(response);
-    })
-    .catch(err => {
-        console.error(err);
-    });
+// fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=London%2Cuk&lat=0&lon=0&callback=test&id=2172797&lang=null&units=imperial&mode=xml", {
+//     "method": "GET",
+//     "headers": {
+//         "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+//         "x-rapidapi-key": "8014487f1emshaa274c05a26e32ap1b8b54jsncaca7949dc19"
+//     }
+// })
+//     .then(response => {
+//         console.log(response);
+//     })
+//     .catch(err => {
+//         console.error(err);
+//     });
 
-fetch("https://community-open-weather-map.p.rapidapi.com/forecast?q=san%20francisco%2Cus", {
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-        "x-rapidapi-key": "8014487f1emshaa274c05a26e32ap1b8b54jsncaca7949dc19"
-    }
-})
-    .then(response => {
-        console.log(response);
-    })
-    .catch(err => {
-        console.error(err);
-    });
+// fetch("https://community-open-weather-map.p.rapidapi.com/forecast?q=san%20francisco%2Cus", {
+//     "method": "GET",
+//     "headers": {
+//         "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+//         "x-rapidapi-key": "8014487f1emshaa274c05a26e32ap1b8b54jsncaca7949dc19"
+//     }
+// })
+//     .then(response => {
+//         console.log(response);
+//     })
+//     .catch(err => {
+//         console.error(err);
+//     });
