9f7c10bb3941d57b3b77310de53590c49fdd4a84
Carson <carsoncolgateccu@gmail.com>
2021-10-06 11:47:50 -0600

    added an event listener on search that returns the url of the city searched

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,25 @@
 var APIKey = "&appid=348695b7be825684de6066f96d8b32f6";
 var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=";
-var apiURLonecallb = "https://api.openweathermap.org/data/2.5/onecall?";
+var fiveday = "https://api.openweathermap.org/data/2.5/forecast?q=";
+var unitMeasurement = "&units=imperial";
 
-var history = JSON.parse(window.localStorage.getItem("city-input")) || {};
+var history = JSON.parse(window.localStorage.getItem("city-input")) || [];
 
 searchContainer = document.querySelector("#container");
 searchButton = document.querySelector("#searchbtn");
 currentWeather = document.querySelector("#currentWeather");
 fiveDay = document.querySelector("#fiveDayForecast");
 cityText = document.querySelector("#city-input");
+
+searchButton.addEventListener('click', search);
+
+function search(e) {
+    userCity = cityText.value;
+    currentDay(e);
+}
+
+function currentDay(e) {
+    e.preventDefault();
+    finalUrl = apiURL + userCity + APIKey + unitMeasurement;
+    console.log(finalUrl)
+}
